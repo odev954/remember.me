91199a2aa75257b319fc40e4abaf51b9c4deee23
odev954 <kleinbottle175@gmail.com>
2023-01-03 22:39:38 +0200

    adding queries to app service

--- backend/src/lambdas/notesManager/src/app.service.ts
+++ backend/src/lambdas/notesManager/src/app.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import Note from './common/note';
+import * as queries from './queries';
+import * as aws from 'aws-sdk';
+import { Result } from './common/result';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private database: aws.DynamoDB.DocumentClient;
+
+  public constructor() {
+    this.database = new aws.DynamoDB.DocumentClient();
+  }
+
+  async getNotes(): Promise<Result<Note[]>> {
+    return queries.getNotes(this.database);
+  }
+
+  async createNote(note: Note): Promise<Result> {
+    return queries.createNote(this.database, note);
+  }
+
+  async updateNote(note: Note): Promise<Result> {
+    return queries.updateNote(this.database, note);
+  }
+
+  async deleteNote(noteId: string): Promise<Result> {
+    return queries.deleteNote(this.database, noteId);
   }
 }
