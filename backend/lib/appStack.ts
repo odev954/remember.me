7cdf1f0a5f6064467202774316c7a9eee90b9b81
odev954 <kleinbottle175@gmail.com>
2023-01-04 11:44:02 +0200

    updating app stack

--- backend/lib/appStack.ts
+++ backend/lib/appStack.ts
@@ -1,8 +1,15 @@
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import { Construct } from "constructs";
+import {
+	Duration,
+	Stack,
+	StackProps,
+	Expiration,
+	Construct,
+} from "@aws-cdk/core";
+import * as apigateway from "@aws-cdk/aws-apigateway";
+import * as lambda from "@aws-cdk/aws-lambda";
 import * as path from "path";
+import * as appsync from "@aws-cdk/aws-appsync";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
 
 export default class AppStack extends Stack {
 	constructor(scope: Construct, id: string, props?: StackProps) {
@@ -10,18 +17,79 @@ export default class AppStack extends Stack {
 
 		const notesManagerFunction = new lambda.Function(this, "NotesManager", {
 			runtime: lambda.Runtime.NODEJS_16_X,
-			handler: "index.handler",
-			code: lambda.Code.fromAsset(path.join(__dirname, "lambda-handler")),
+			handler: "main.handler",
+			code: lambda.Code.fromAsset(
+				path.join(__dirname, "lambdas/notesManager/src")
+			),
 		});
 
-		const api = new apigateway.RestApi(this, "NotesApi");
+		const rest = new apigateway.RestApi(this, "NotesApi");
+		const graphql = new appsync.GraphqlApi(this, "Api", {
+			name: "cdk-notes-api",
+			schema: appsync.Schema.fromAsset("../data/scheme.graphql"),
+			xrayEnabled: true,
+		});
 
-		api.root.addMethod("ANY");
+		const datasource = graphql.addLambdaDataSource(
+			"notesManagerDS",
+			notesManagerFunction
+		);
 
-		const notes = api.root.addResource("notes");
+		const notesTable = new dynamodb.Table(this, "NotesTable", {
+			billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+			partitionKey: {
+				name: "id",
+				type: dynamodb.AttributeType.STRING,
+			},
+		});
+
+		datasource.createResolver({
+			typeName: "Mutation",
+			fieldName: "createNote",
+		});
+
+		datasource.createResolver({
+			typeName: "Mutation",
+			fieldName: "deleteNote",
+		});
+
+		datasource.createResolver({
+			typeName: "Mutation",
+			fieldName: "updateNote",
+		});
+
+		datasource.createResolver({
+			typeName: "Query",
+			fieldName: "getNotes",
+		});
+
+		notesTable.grantFullAccess(notesManagerFunction);
+		notesManagerFunction.addEnvironment(
+			"NOTES_TABLE",
+			notesTable.tableName
+		);
+
+		rest.root.addMethod("ANY");
+
+		const notes = rest.root.addResource("notes");
 
 		notes.addMethod(
 			"GET",
+			new apigateway.LambdaIntegration(notesManagerFunction)
+		);
+
+		notes.addMethod(
+			"PUT",
+			new apigateway.LambdaIntegration(notesManagerFunction)
+		);
+
+		notes.addMethod(
+			"POST",
+			new apigateway.LambdaIntegration(notesManagerFunction)
+		);
+
+		notes.addMethod(
+			"DELETE",
 			new apigateway.LambdaIntegration(notesManagerFunction)
 		);
 	}
