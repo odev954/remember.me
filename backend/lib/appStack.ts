9396b2c28a155c3e8d5549609006fb8dce9484e4
odev954 <kleinbottle175@gmail.com>
2023-01-04 10:01:50 +0200

    setting up code base in cdk project for backend

---
+++ backend/lib/appStack.ts
@@ -0,0 +1,28 @@
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import * as path from "path";
+
+export default class AppStack extends Stack {
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
+
+		const notesManagerFunction = new lambda.Function(this, "NotesManager", {
+			runtime: lambda.Runtime.NODEJS_16_X,
+			handler: "index.handler",
+			code: lambda.Code.fromAsset(path.join(__dirname, "lambda-handler")),
+		});
+
+		const api = new apigateway.RestApi(this, "NotesApi");
+
+		api.root.addMethod("ANY");
+
+		const notes = api.root.addResource("notes");
+
+		notes.addMethod(
+			"GET",
+			new apigateway.LambdaIntegration(notesManagerFunction)
+		);
+	}
+}
