2324d07f08465629e2c15ecf23d9733be3fa6290
odev954 <kleinbottle175@gmail.com>
2023-01-17 13:57:12 +0200

    updating routes in microservice

--- backend/lib/lambdas/notesManager/src/app.controller.ts
+++ backend/lib/lambdas/notesManager/src/app.controller.ts
@@ -14,22 +14,22 @@ import Note from './common/note';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post()
+  @Post('/notes')
   async createNote(@Body() note: Note) {
     return await this.appService.createNote(note);
   }
 
-  @Get()
+  @Get('/notes')
   async getNotes() {
     return await this.appService.getNotes();
   }
 
-  @Delete(':id')
+  @Delete('/notes/:id')
   async deleteNote(@Param('id') id: string) {
     return await this.appService.deleteNote(id);
   }
 
-  @Put(':id')
+  @Put('/notes/:id')
   async updateNote(@Param('id') id: string, @Body() note: Note) {
     note.id = id;
     return await this.appService.updateNote(note);
--- backend/lib/lambdas/notesManager/src/queries/updateNote.ts
+++ backend/lib/lambdas/notesManager/src/queries/updateNote.ts
@@ -4,27 +4,27 @@ import { Result } from '../common/result';
 import { StatusCodes } from '../common/statusCodes';
 
 export default async function updateNote(
-    database: aws.DynamoDB.DocumentClient,
-    note: Note,
+  database: aws.DynamoDB.DocumentClient,
+  note: Note,
 ): Promise<Result> {
-    note.updated_at = Math.floor(Date.now() / 1000); //set time to unix epoch
-    const params = {
-        TableName: process.env.NOTES_TABLE,
-        Key: {
-            id: note.id,
-        },
-        UpdateExpression: 'set content = :content, title = :title',
-        ExpressionAttributeValues: {
-            ':content': note.content,
-            ':title': note.title,
-        },
-    };
+  note.updated_at = Math.floor(Date.now() / 1000); //set time to unix epoch
+  const params = {
+    TableName: process.env.NOTES_TABLE,
+    Key: {
+      id: note.id,
+    },
+    UpdateExpression: 'set content = :content, title = :title',
+    ExpressionAttributeValues: {
+      ':content': note.content,
+      ':title': note.title,
+    },
+  };
 
-    try {
-        await database.update(params).promise();
-        return { status: StatusCodes.Success };
-    } catch (error) {
-        console.log('note update failed, aborted by db client. cause: ', error);
-        return { status: StatusCodes.Failure };
-    }
+  try {
+    await database.update(params).promise();
+    return { status: StatusCodes.Success };
+  } catch (error) {
+    console.log('note update failed, aborted by db client. cause: ', error);
+    return { status: StatusCodes.Failure };
+  }
 }
